cf8b48ca4be5c9e77833b73f16a12b3d9d61e660
Admin <soderstromshane@gmail.com>
2017-12-12 17:25:30 -0700

    changed the image url in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 10.times do |d|
-FreelanceDocument.create!(title: "Document #{d}", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.", file_url: "https://jobmob.co.il/files/2014/11/freelancer-logo.png", image_url: "https://cdn.pixabay.com/photo/2014/12/22/19/59/macbook-577758_960_720.jpg")
+FreelanceDocument.create!(title: "Document #{d}", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.", file_url: "https://jobmob.co.il/files/2014/11/freelancer-logo.png", image_url: "https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg")
 end
 puts "10 Documents created!"
